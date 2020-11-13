6b5eb80ed4e6b550d87be056fc8157e8237e9d2a
Hari Wiguna <hwiguna@gmail.com>
2020-11-12 23:46:55 -0600

    Update Magic.ino
    Good night

--- Magic/Magic.ino
+++ Magic/Magic.ino
@@ -114,3 +114,14 @@ void loop() {
   PickaNumber();
   NumberPicked();
 }
+
+/*
+ * [A] User pick a secret number between 1 and 99
+ * [B] Magician pick a random sentinel
+ * [B] Magician decide how many guesses before showing secret#
+ * [C] Magician fills guessNumbers array.  Last element is the secret#, before that is the sentinel
+ * [D] Magician displays sentinel hint as first guess
+ * [F] Magician waits for accomplice response in button 3
+ * [G] Magician keeps displaying prepared guess array and waiting for response
+ */
+ */
