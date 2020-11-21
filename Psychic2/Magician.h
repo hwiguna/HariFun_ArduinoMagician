b841c43f4bc786a251d987b4744b241ee5616f77
Hari Wiguna <hwiguna@gmail.com>
2020-11-21 12:34:40 -0600

    Psychic just say no :-)

---
+++ Psychic2/Magician.h
@@ -0,0 +1,225 @@
+
+// States of the magic trick
+#define HAS_NOTHING 0
+#define HAS_HINT 1
+#define HAS_SENTINEL 2
+#define HAS_SEEN_SENTINEL 3
+//#define HAS_CHOSENNUMBER 4
+//#define HAS_COMPLETED 5
+#define PAUSE_FOR_EFFECT 1000
+byte magicState;
+
+const byte maxNumberOfGuesses = 9;
+byte guesses[maxNumberOfGuesses];
+byte numberOfGuesses;
+byte sentinelHint; // This is the clue to the sentinel
+
+byte sentinelDigit; // The sentinel digit we're going to look for
+bool isSentinelOnLeft; // Is sentinel digit on left or right?
+byte sentinelNumber; // This is the sentinel (only one of the digits is relevant)
+
+byte chosenNumber;
+
+void Print(String label, byte number) {
+  Serial.print(label);
+  Serial.print(" = ");
+  Serial.println(number);
+}
+
+void PrintElement(String var, byte index, byte value) {
+  Serial.print(var);
+  Serial.print("[");
+  Serial.print(index);
+  Serial.println(value);
+}
+
+byte HintToSentinel(byte sentinelHint) {
+
+  byte firstDigit = sentinelHint / 10;
+  byte secondDigit = sentinelHint % 10;
+  isSentinelOnLeft = (firstDigit > secondDigit);
+  byte sentinelHintDigit =  isSentinelOnLeft ? firstDigit : secondDigit;
+  sentinelDigit = 9 - sentinelHintDigit; // This is what indicates that next # is the secret #
+
+  do {
+    byte sentinelOtherDigit = random(0, 10); // any digit will do. Even same as sentinel digit.
+
+    sentinelNumber = (isSentinelOnLeft) ?
+                     sentinelDigit * 10 + sentinelOtherDigit :
+                     sentinelOtherDigit * 10 + sentinelDigit;
+  } while (sentinelNumber == 0);
+
+  Print("HintToSentinel. sentinelHint", sentinelHint);
+  Print("firstDigit", firstDigit);
+  Print("secondDigit", secondDigit);
+  Print("isSentinelOnLeft", isSentinelOnLeft);
+  Print("sentinelHintDigit", sentinelHintDigit);
+  Print("sentinelDigit", sentinelDigit);
+  Print("sentinelNumber", sentinelNumber);
+
+  return sentinelNumber;
+}
+
+byte PickHint() {
+  byte firstDigit, secondDigit;
+  do {
+    sentinelHint = random(1, maxNumber + 1);
+    firstDigit = sentinelHint / 10;
+    secondDigit = sentinelHint % 10;
+  } while (firstDigit == secondDigit); // Hint must be made of two different digits to indicate which one is the sentinel digit.
+  return sentinelHint;
+}
+
+byte GoodRandom() {
+  byte randomGuess;
+  bool conflictWithSentinelDigit, conflictWithSentinelHint, conflictWithChosenNumber;
+  do {
+    randomGuess = random(1, maxNumber + 1);
+    byte valueAtSentinelPosition = isSentinelOnLeft ? randomGuess / 10 : randomGuess % 10;
+    conflictWithSentinelDigit = valueAtSentinelPosition == sentinelDigit;
+    conflictWithSentinelHint = randomGuess == sentinelHint;
+    conflictWithChosenNumber = randomGuess == chosenNumber;
+  } while (conflictWithSentinelDigit || conflictWithSentinelHint || conflictWithChosenNumber );
+  return randomGuess;
+}
+
+void GenerateGuesses() {
+  Serial.println("GenerateGuesses");
+  guesses[0] = sentinelHint; // First guess is the sentinel hint
+  guesses[numberOfGuesses - 2] = sentinelNumber; // next to last guess is the sentinel #
+  guesses[numberOfGuesses - 1] = chosenNumber; // last guess is the chosen#
+
+  // Fill the rest of guesses randomly
+  for (byte i = 1; i < (numberOfGuesses - 2); i++) {
+    guesses[i] = GoodRandom();
+    PrintElement("guesses", i, guesses[i]);
+  }
+}
+
+//------------------------------------
+
+//void PrepareMagic() {
+//  Print("\nPrepareMagic. chosenNumber", chosenNumber);
+//  numberOfGuesses = random(3, maxNumberOfGuesses + 1);
+//  Print("numberOfGuesses", numberOfGuesses);
+//
+//  do {
+//    sentinelHint = PickHint();
+//    sentinelNumber = HintToSentinel(sentinelHint);
+//  } while ((sentinelHint == chosenNumber) || (sentinelNumber == chosenNumber));
+//
+//  Print("chosenNumber", chosenNumber);
+//  Print("sentinelHint", sentinelHint);
+//  Print("sentinelNumber", sentinelNumber);
+//
+//  GenerateGuesses();
+//  currentState = WAITING_FOR_GUESS;
+//}
+
+void PerformMagic() {
+  Serial.println("PerformMagic");
+  //  // First, show "--" to indicate start of guesses...
+  //  currentNumber = 0;
+  //  byte pressedButtonIndex;
+  //  do {
+  //    pressedButtonIndex = WaitForButton();
+  //    if (pressedButtonIndex == 0) {currentState = WAITING_FOR_CHOSEN_NUMBER; break;}
+  //    if (pressedButtonIndex == 2) {currentState = WAITING_FOR_NEXT_GUESS; break;}
+  //  } while (true);
+  //
+  //  if (currentState == WAITING_FOR_NEXT_GUESS) {
+  //    if (numberOfGuesses != 0) { // Make sure PrepareMagic() has filled the guesses array()
+  //      for (byte i = 0; i < numberOfGuesses; i++) {
+  //        Serial.print("Guess["); Serial.print(i); Print("]", guesses[i]);
+  //      }
+  //      for (byte guessIndex = 0; guessIndex < numberOfGuesses; guessIndex++) {
+  //        PrintElement("guesses", guessIndex, guesses[guessIndex]);
+  //        currentNumber = guesses[guessIndex];
+  //        do {
+  //          //Refresh();
+  //          delay(100);
+  //        } while (digitalRead(buttonPins[2]) != PRESSED);
+  //
+  //        delay(500);
+  //      }
+  //
+  //      currentState = WAITING_FOR_NEXT_TRICK;
+  //      Applause(); // until they press button 0 to get another random#
+  //      numberOfGuesses = 0; // Get ready for next show
+  //      chosenNumber = 0;
+  //    }
+  //  }
+}
+
+void ProcessFirstDigit(char keypress) {
+
+  if (keypress >= '0' && keypress <= '9') {
+    currentNumber = (keypress - '0') * 10;
+    isDisplayOn[0] = false; // ones
+    isDisplayOn[1] = true;  // tens
+    currentState = WAITING_FOR_SECOND_DIGIT;
+  }
+}
+
+void SaveSentinel() {
+  delay(PAUSE_FOR_EFFECT); // Pause for effect before showing "No"
+  sentinelHint = currentNumber;
+  magicState = HAS_HINT;
+  Print("HAS_HINT. sentinelHint", sentinelHint);
+
+  sentinelNumber = HintToSentinel(sentinelHint);
+
+  magicState = HAS_SENTINEL;
+  Print("HAS_SENTINEL. sentinelDigit", sentinelDigit);
+  Print("isSentinelOnLeft", isSentinelOnLeft);  // We will be looking for sentinel on left/right
+
+}
+
+void CheckForSentinel(byte currentNumber) {
+  Print("CheckForSentinel. currentNumber", currentNumber);
+  byte potentialSentinelDigit = isSentinelOnLeft ? currentNumber / 10 : currentNumber % 10;
+  if (potentialSentinelDigit == sentinelDigit) {
+    magicState = HAS_SEEN_SENTINEL;
+    Print("HAS_SEEN_SENTINEL! currentNumber", currentNumber);
+  }
+}
+
+void ProcessSecondDigit(char keypress) {
+  Print("ProcessSecondDigit. keypress char",keypress);
+  if (keypress >= '0' && keypress <= '9') {
+    currentNumber = currentNumber + (keypress - '0');
+    isDisplayOn[0] = true; // ones
+    isDisplayOn[1] = true;  // tens
+    currentState = WAITING_FOR_FIRST_DIGIT;
+    Serial.println("Waiting for effect");
+    justSayNo = false;
+    delay(PAUSE_FOR_EFFECT); // Pause for effect before showing "No" or applause
+    justSayNo = true;
+    Serial.println("DONE Waiting for effect");
+
+    Print("magicState",magicState);
+    switch (magicState) {
+      case HAS_NOTHING: SaveSentinel(); break;
+      case HAS_SENTINEL: CheckForSentinel(currentNumber); break;
+      case HAS_SEEN_SENTINEL: // which means we're showing the chosen# now.
+        // Say no to sentinel as well
+        //magicState = HAS_CHOSENNUMBER;
+        justSayNo = false;
+        Applause();
+        break;
+//      case HAS_CHOSENNUMBER: 
+//        Applause(); break;
+    }
+  }
+}
+
+void ProcessKeypress(char keypress) {
+  switch (currentState) {
+    case WAITING_FOR_FIRST_DIGIT:
+      ProcessFirstDigit(keypress);
+      break;
+    case WAITING_FOR_SECOND_DIGIT:
+      ProcessSecondDigit(keypress);
+      break;
+  }
+}
---
+++ Psychic2/Psychic2.ino
@@ -0,0 +1,244 @@
+#include <TimerOne.h>
+
+/*
+   Knows what was picked by another Arduino!
+   by Hari Wiguna, 2020
+*/
+
+const byte segmentPins[] = {2, 3, 4, 5, 6, 7, 8};
+const byte digitPins[] = {12, 13};
+const byte maxNumber = 99; // Max # that could be displayed with two digits
+const byte OFF = HIGH;
+const byte ON = LOW;
+const byte PRESSED = LOW;
+
+const byte numberMap[] = {
+  //.GFEDCBA
+  B00111111,  // 0
+  B00000110,  // 1
+  B01011011,  // 2
+  B01001111,  // 3
+  B01100110,  // 4
+  B01101101,  // 5
+  B01111101,  // 6
+  B00000111,  // 7
+  B01111111,  // 8
+  B01101111,  // 9
+  B01000000,  // -
+  B00110111,  // N
+  B01011100,  // o
+  //B01010011,  // ?
+};
+
+byte currentNumber = 0; //random(1, maxNumber + 1);
+volatile byte currentDigitIndex = 0;
+volatile bool isDisplayOn[2] = {true, true};
+
+// Data entry states:
+#define WAITING_FOR_FIRST_DIGIT  0
+#define WAITING_FOR_SECOND_DIGIT 1
+volatile byte currentState = WAITING_FOR_FIRST_DIGIT;
+
+volatile bool justSayNo;
+
+//=== Keypad ===
+const byte rowPowerPins[] = {A4, A5, 9, 10};
+const byte colReadPins[] = {A0, A1, A2, A3};
+const char keypadMap[] = {'1', '2', '3', 'A', '4', '5', '6', 'B', '7', '8', '9', 'C', '*', '0', '#', 'D'};
+
+void DisplayCurrentNumber() {
+  digitalWrite(digitPins[currentDigitIndex], OFF); // Turn off last digit index
+  currentDigitIndex = 1 - currentDigitIndex; // alternate digit index
+  byte currentDigit = (currentDigitIndex == 0) ? currentNumber % 10 : currentNumber / 10;
+  if (currentState == WAITING_FOR_FIRST_DIGIT) {
+    if (currentNumber == 0) currentDigit = 10; // Dash  
+    if (justSayNo) {
+      currentDigit = (currentDigitIndex == 0) ? 12 /*o*/ : 11/*N*/; // No!
+    }
+  }
+  
+  for (byte i = 0; i < 7; i++) {
+    bool isOn = !(bitRead(numberMap[currentDigit], i) && isDisplayOn[currentDigitIndex]);
+    digitalWrite(segmentPins[i], isOn);
+  }
+  digitalWrite(digitPins[currentDigitIndex], ON);
+}
+
+void SetupKeypad() {
+  for (byte r = 0; r < 4; r++) {
+    pinMode(rowPowerPins[r], OUTPUT);
+    digitalWrite(rowPowerPins[r], HIGH);
+  }
+  for (byte c = 0; c < 4; c++) {
+    pinMode(colReadPins[c], INPUT_PULLUP);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  for (byte i = 0; i < 7; i++) {
+    pinMode(segmentPins[i], OUTPUT);
+    digitalWrite(segmentPins[i], OFF);
+  }
+  for (byte i = 0; i < 2; i++) {
+    pinMode(digitPins[i], OUTPUT);
+    digitalWrite(digitPins[i], ON);
+  }
+
+  SetupKeypad();
+
+  Timer1.initialize(10000); // call every 0.01 seconds
+  Timer1.attachInterrupt(DisplayCurrentNumber);
+}
+
+void TestSegments() {
+  digitalWrite(digitPins[0], LOW); // common anode off
+  digitalWrite(digitPins[1], LOW); // common anode off
+  for (byte d = 0; d < 2; d++) {
+    digitalWrite(digitPins[d], HIGH); // common anode on
+    for (byte i = 0; i < 7; i++) {
+      digitalWrite(segmentPins[i], ON);
+      delay(150);
+      digitalWrite(segmentPins[i], OFF);
+      delay(150);
+    }
+    digitalWrite(digitPins[d], LOW); // common anode off
+  }
+}
+
+void TestDigits() {
+  digitalWrite(digitPins[0], ON); digitalWrite(digitPins[1], OFF); delay(500);
+  digitalWrite(digitPins[0], OFF); digitalWrite(digitPins[1], ON); delay(500);
+}
+
+void TestNumbers() {
+  digitalWrite(digitPins[0], ON); digitalWrite(digitPins[1], OFF);
+  for (byte ni = 0; ni < 10; ni++) {
+    for (byte i = 0; i < 7; i++) {
+      digitalWrite(segmentPins[i], !bitRead(numberMap[ni], i));
+    }
+    delay(500);
+  }
+}
+
+void Refresh() {
+  //  digitalWrite(digitPins[currentDigitIndex], OFF); // Turn off last digit index
+  //  currentDigitIndex = 1 - currentDigitIndex; // alternate digit index
+  //  byte currentDigit = (currentDigitIndex == 0) ? currentNumber % 10 : currentNumber / 10;
+  //  if (currentNumber == 0) currentDigit = 10; // Dash
+  //  for (byte i = 0; i < 7; i++) {
+  //    digitalWrite(segmentPins[i], !bitRead(numberMap[currentDigit], i));
+  //  }
+  //  digitalWrite(digitPins[currentDigitIndex], ON);
+  DisplayCurrentNumber();
+  delay(10);
+}
+
+void ShuffleRandomNumber() {
+  //  if (digitalRead(buttonPins[0]) == PRESSED) {
+  //    byte randomNumber = random(1, 100);
+  //    currentNumber = randomNumber;
+  //  }
+}
+
+
+#include "Magician.h"
+
+
+byte ReadKeypad() {
+  byte keyPressed = 99;
+  for (byte r = 0; r < 4; r++) {
+    //Print("\nr", r);
+    digitalWrite(rowPowerPins[r], LOW);
+    for (byte c = 0; c < 4; c++) {
+      //Print("c", c);
+      if (digitalRead(colReadPins[c]) == 0) {
+        keyPressed = r * 4 + c;
+        //Print("keyPressed", keyPressed);
+      }
+    }
+    digitalWrite(rowPowerPins[r], HIGH);
+    //if (keyPressed != 99) break;
+  }
+  return keyPressed;
+}
+
+//bool NumberPicked() {
+//  if ((currentNumber != 0) && (digitalRead(buttonPins[1]) == PRESSED)) {
+//    chosenNumber = currentNumber;
+//  }
+//  return chosenNumber != 0;
+//}
+
+void Applause() {
+  Serial.println("Applause");
+    bool buttonPressed = false;
+    do {
+      isDisplayOn[0] = !isDisplayOn[0];
+      isDisplayOn[1] = !isDisplayOn[1];
+      for (byte i = 0; i < 5; i++) {
+        byte keyPressedIndex = ReadKeypad();
+        if (keyPressedIndex != 99) {
+          buttonPressed = true;
+        }
+        delay(50);
+      }
+    } while (!buttonPressed);
+  isDisplayOn[0] = true;
+  isDisplayOn[1] = true;
+
+  currentNumber = 0; // back to "--"
+  currentState = WAITING_FOR_FIRST_DIGIT;
+  magicState = HAS_NOTHING;
+  delay(1000);
+}
+
+void TestKeypad() {
+  byte keyPressedIndex = ReadKeypad();
+  currentNumber = keyPressedIndex;
+  if (keyPressedIndex != 99) {
+    char ch = keypadMap[keyPressedIndex];
+    //Print("ch", ch);
+    Serial.println(ch);
+
+    while (ReadKeypad() == keyPressedIndex)
+      delay(250);
+  }
+}
+
+char WaitForKeypress() {
+  byte keyPressedIndex;
+  // Wait for a key to be pressed
+  do {
+    keyPressedIndex = ReadKeypad();
+  } while (keyPressedIndex == 99);
+
+  // Wait for that key to be released
+  while (ReadKeypad() == keyPressedIndex)
+    delay(250);
+
+  char ch = keypadMap[keyPressedIndex];
+  Serial.println(ch);
+
+  return ch;
+}
+
+void loop() {
+  //TestSegments();
+  //TestKeypad();
+  //TestDigits();
+  //TestButtons();
+  //TestNumbers();
+  //Refresh();
+  //
+  switch (currentState) {
+    case WAITING_FOR_FIRST_DIGIT:
+      ProcessKeypress( WaitForKeypress() );
+      break;
+    case WAITING_FOR_SECOND_DIGIT:
+      ProcessKeypress( WaitForKeypress() );
+      break;
+  }
+
+}
